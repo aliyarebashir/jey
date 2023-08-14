e3785c5ce72cb26c95db569f14a921110865e71c
uwes <uwess529300@gmail.com>
2023-08-14 22:46:52 +0300

    addded hamberger

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ let planButton =document.querySelectorAll(".button")
 let modal=document.querySelector(".modal")
 let backdrop=document.querySelector('.backdrop')
 let negativeButton=document.querySelector(".no_button")
+let toggleButton=document.querySelector(".toggle-button")
+let mobileNav=document.querySelector(".mobile_header_nav")
 function closeModal(){
     backdrop.style.display="none";
     modal.style.display="none";
@@ -13,5 +15,13 @@ planButton[i].addEventListener("click"  ,function(){
     modal.style.display="block";
 })
 
-backdrop.addEventListener('click',closeModal)
-negativeButton.addEventListener('click',closeModal)
+backdrop.addEventListener('click',function(){
+    mobileNav.style.display="none"
+    closeModal()
+})
+if(negativeButton){negativeButton.addEventListener('click',closeModal)}
+toggleButton.addEventListener("click",function(){
+    backdrop.style.display="block";
+    mobileNav.style.display="block"
+
+})
